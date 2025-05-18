06056117908995d7357e3604ac7b5a6681d882b0
Vinayak Ghatul <vinayakghatul@gmail.com>
2025-05-18 20:15:44 +0530

    Repeating and Missing number

--- StriverDSA/src/Missing_Repeating_Number.java
+++ StriverDSA/src/Missing_Repeating_Number.java
@@ -1,11 +1,17 @@
 import java.util.*;
 
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList; // Import ArrayList
-import java.util.Stack;
+/**
+ * Problem Statement: You are given a read-only array of N integers with
+ * values also in the range [1, N] both inclusive. Each integer appears exactly
+ * once except A which appears twice and B which is missing.
+ * The task is to find the repeating and missing numbers A and B where A repeats twice and B is missing.
+ *
+ * Example 1:
+ * Input Format:  array[] = {3,1,2,5,3}
+ * Result: {3,4)
+ * Explanation: A = 3 , B = 4
+ * Since 3 is appearing twice and 4 is missing
+ */
 
 class Missing_Repeating_Number {
     // Function to find two elements in array
