420c56105b1b95c254e089b6951cb77b6f299ee7
Vinayak Ghatul <vinayakghatul@gmail.com>
2025-05-21 14:15:49 +0530

    Merge two sorted lists

---
+++ StriverDSA/src/Merge_Two_Lists.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+/**
+ * Iterative approach
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Merge_Two_Lists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // create one dummy node to begin
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while(list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 == null) {
+            curr.next = list2;
+        } else {
+            curr.next = list1;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String args[]) {
+        // Create list1: 1 -> 3 -> 5
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+        // Create list2: 2 -> 4 -> 6
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        // Merge lists
+        Merge_Two_Lists solution = new Merge_Two_Lists();
+        ListNode merged = solution.mergeTwoLists(list1, list2);
+
+        // Print merged list
+        System.out.print("Merged List: ");
+        while (merged != null) {
+            System.out.print(merged.val + " ");
+            merged = merged.next;
+        }
+    }
+}
