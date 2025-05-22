30ed1e200fa709e5d8615053b4cb70c16d1dd1d6
Vinayak Ghatul <vinayakghatul@gmail.com>
2025-05-22 13:47:47 +0530

    Intersection of two lists

--- StriverDSA/src/Intersection_Two_Lists.java
+++ StriverDSA/src/Intersection_Two_Lists.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/**
+ * Given the heads of two singly linked-lists headA and headB,
+ * return the node at which the two lists intersect.
+ * If the two linked lists have no intersection at all, return null.
+ */
+
 
 class ListNode {
     int val;
