4079d55bf380f73422401387ccaf5dc438046c3b
Vinayak Ghatul <vinayakghatul@gmail.com>
2025-05-25 19:56:38 +0530

    Max width of binary tree

--- StriverDSA/src/Max_Width_Binary_Tree.java
+++ StriverDSA/src/Max_Width_Binary_Tree.java
@@ -1,5 +1,21 @@
 import java.util.*;
 
+/**
+ * Given the root of a binary tree, return the maximum width of the given tree.
+ *
+ * The maximum width of a tree is the maximum width among all levels.
+ *
+ * The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes),
+ * where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level
+ * are also counted into the length calculation.
+ *
+ *          1
+ *        /   \
+ *       3     2
+ *      / \     \
+ *     5   3     9
+ */
+
 class TreeNode {
     int val;
     TreeNode left;
