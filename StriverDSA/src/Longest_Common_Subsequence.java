249a6897b05f47113767602feda09246bad52f1c
Vinayak Ghatul <vinayakghatul@gmail.com>
2025-05-27 12:20:46 +0530

    Longest common subsequence

--- StriverDSA/src/Longest_Common_Subsequence.java
+++ StriverDSA/src/Longest_Common_Subsequence.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
+/**
+ * Given two strings text1 and text2, return the length of their longest common subsequence.
+ * If there is no common subsequence, return 0.
+ *
+ * A subsequence of a string is a new string generated from the original string with some characters
+ * (can be none) deleted without changing the relative order of the remaining characters.
+ *
+ * For example, "ace" is a subsequence of "abcde".
+ * A common subsequence of two strings is a subsequence that is common to both strings.
+ *
+ * Example 1:
+ *
+ * Input: text1 = "abcde", text2 = "ace"
+ * Output: 3
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ */
+
 
 class Longest_Common_Subsequence {
 
