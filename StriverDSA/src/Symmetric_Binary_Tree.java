5d7fdb0b1472c21a3a77fa7e0f43b93a48f15230
Vinayak Ghatul <vinayakghatul@gmail.com>
2025-05-29 18:42:16 +0530

    Symmetric binary tree

---
+++ StriverDSA/src/Symmetric_Binary_Tree.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Symmetric_Binary_Tree {
+
+    public boolean isSymmetricUtil(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null || root2 == null) {
+            return root1 == root2;
+        }
+
+        return (root1.val == root2.val) && isSymmetricUtil(root1.left, root2.right) && isSymmetricUtil(root1.right, root2.left);
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricUtil(root.left, root.right);
+    }
+
+    public static void main(String args[]) {
+        Symmetric_Binary_Tree treeChecker = new Symmetric_Binary_Tree();
+
+        // Example of a symmetric tree:
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println("Is tree symmetric? " + treeChecker.isSymmetric(root)); // true
+
+        // Example of an asymmetric tree:
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(2);
+        root2.left.right = new TreeNode(3);
+        root2.right.right = new TreeNode(3);
+
+        System.out.println("Is tree symmetric? " + treeChecker.isSymmetric(root2)); // false
+    }
+
+}
